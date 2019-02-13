0b853db4e451302e82e97db89b279c7c1af18a9d
code Block <codeBlock@iceony.org>
2019-02-13 17:17:15 +0100

    [ feature #163843095 ] create api modify meal endpoint

--- API/services/mealsService.js
+++ API/services/mealsService.js
@@ -1,4 +1,4 @@
-//import { validationResult } from 'express-validator/check';
+// import { validationResult } from 'express-validator/check';
 import meals from '../models/meals';
 
 const mealsData = meals;
@@ -14,6 +14,25 @@ class mealsService {
     return res.status(200).json({
       status: 200,
       data: mealsData,
+    });
+  }
+
+  static editMeal(req, res) {
+    const mealID = parseInt(req.params.id, 10);
+    const newMeal = req.body;
+    const singleMeal = mealsData.find(meal => meal.mealID === mealID);
+
+    if (singleMeal) {
+      if (newMeal.mealName) singleMeal.mealName = newMeal.mealName;
+      if (newMeal.mealPrice) singleMeal.mealPrice = newMeal.mealPrice;
+      return res.status(200).json({
+        status: 200,
+        data: singleMeal,
+      });
+    }
+    return res.status(404).json({
+      status: 404,
+      error: 'Meal with specified ID not found!',
     });
   }
 }
