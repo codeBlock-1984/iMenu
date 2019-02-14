a477687ce54d4642037b5bf00d4a2a9bc7c73f6f
code Block <codeBlock@iceony.org>
2019-02-14 08:44:45 +0100

    [ feature #163971546 ] create api retrieve order by ID endpoint

--- API/routes/ordersRoute.js
+++ API/routes/ordersRoute.js
@@ -2,10 +2,14 @@ import express from 'express';
 import ordersService from '../services/ordersService';
 
 const router = express.Router();
-const { placeOrder, getOrdersDay, modifyOrder } = ordersService;
+const {
+  placeOrder, getOrdersDay, modifyOrder, getOrder,
+} = ordersService;
 
 router.post('/', placeOrder);
 router.get('/:date', getOrdersDay);
+router.get('/:id', getOrder);
 router.put('/:id', modifyOrder);
+
 
 export default router;
