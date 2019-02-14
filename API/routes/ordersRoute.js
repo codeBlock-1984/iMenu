0c5d4f0cd7f23f21c76d148d6e552f90b0aee7ec
code Block <codeBlock@iceony.org>
2019-02-14 07:52:00 +0100

    [ feature #163843130 ] create api modify order endpoint

--- API/routes/ordersRoute.js
+++ API/routes/ordersRoute.js
@@ -2,9 +2,10 @@ import express from 'express';
 import ordersService from '../services/ordersService';
 
 const router = express.Router();
-const { placeOrder, getOrdersDay } = ordersService;
+const { placeOrder, getOrdersDay, modifyOrder } = ordersService;
 
 router.post('/', placeOrder);
 router.get('/:date', getOrdersDay);
+router.put('/:id', modifyOrder);
 
 export default router;
